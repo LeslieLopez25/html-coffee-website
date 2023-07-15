2ac64b84bd5d92e2be93830f45b6b84f195084de
Leslie Lopez <leslie2515lopez@gmail.com>
2023-07-14 23:31:10 -0600

    Adding project files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
